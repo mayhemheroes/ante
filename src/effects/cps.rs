858d8fae608adb2abc7f1ce6caf4199accce3104
Jake Fecher <jake@aztecprotocol.com>
2023-04-30 12:23:34 -0500

    CPS more expression types

--- src/effects/cps.rs
+++ src/effects/cps.rs
@@ -1,10 +1,13 @@
-use crate::hir::FunctionType;
-use crate::hir::PrimitiveType;
-use crate::hir::DefinitionId;
-use crate::hir::FunctionCall;
-use crate::hir::DefinitionInfo;
+use std::collections::HashSet;
+
+use crate::hir;
 use crate::hir::Ast;
+use crate::hir::DefinitionId;
+use crate::hir::DefinitionInfo;
+use crate::hir::FunctionCall;
+use crate::hir::FunctionType;
 use crate::hir::Lambda;
+use crate::hir::PrimitiveType;
 use crate::hir::Tuple;
 use crate::hir::Type;
 use crate::hir::Variable;
@@ -20,14 +23,11 @@ pub fn convert_to_cps(ast: Ast) -> Ast {
 #[derive(Default)]
 struct Context {
     next_variable_id: usize,
+    finished_definitions: HashSet<DefinitionId>,
 }
 
 fn make_call(f: Ast, args: Vec<Ast>) -> Ast {
-    Ast::FunctionCall(FunctionCall {
-        function: Box::new(f),
-        args,
-        function_type: todo_function_type(),
-    })
+    Ast::FunctionCall(FunctionCall { function: Box::new(f), args, function_type: todo_function_type() })
 }
 
 fn todo_function_type() -> FunctionType {
@@ -39,46 +39,37 @@ fn todo_function_type() -> FunctionType {
 }
 
 fn make_lambda(arg: Variable, body: Ast) -> Ast {
-    Ast::Lambda(Lambda {
-        args: vec![arg],
-        body: Box::new(body),
-        typ: todo_function_type(),
-    })
+    Ast::Lambda(Lambda { args: vec![arg], body: Box::new(body), typ: todo_function_type() })
 }
 
 impl Context {
     fn cps(&mut self, ast: &Ast, k: Ast) -> Ast {
         match ast {
             l @ Ast::Literal(_) => make_call(k, vec![l.clone()]),
-            v @ Ast::Variable(_) => make_call(k, vec![v.clone()]),
+            Ast::Variable(variable) => self.cps_variable(variable, k),
             Ast::Lambda(lambda) => self.cps_lambda(lambda),
-            Ast::FunctionCall(call) => {
-                match call.function.as_ref() {
-                    Ast::Lambda(lambda) => self.cps_call_lambda(lambda, &call, k),
-                    _ => self.cps_call(&call, k),
-                }
-            }
-            Ast::Definition(_) => todo!(),
-            Ast::If(_) => todo!(),
+            Ast::FunctionCall(call) => match call.function.as_ref() {
+                Ast::Lambda(lambda) => self.cps_call_lambda(lambda, &call, k),
+                _ => self.cps_call(&call, k),
+            },
+            Ast::Definition(definition) => self.cps_definition(definition, k),
+            Ast::If(if_expr) => self.cps_if_expr(if_expr, k),
             Ast::Match(_) => todo!(),
             Ast::Return(return_expr) => self.cps(&return_expr.expression, k),
-            Ast::Sequence(_) => todo!(),
+            Ast::Sequence(sequence) => self.cps_sequence(sequence, k),
             Ast::Extern(_) => todo!(),
             Ast::Assignment(_) => todo!(),
             Ast::MemberAccess(access) => self.cps_member_access(access, k),
             Ast::Tuple(tuple) => self.cps_tuple(tuple, k),
             Ast::ReinterpretCast(_) => todo!(),
-            Ast::Builtin(_) => todo!(),
+            b @ Ast::Builtin(_) => make_call(k, vec![b.clone()]),
         }
     }
 
     fn make_id_function(&mut self) -> Ast {
         self.next_variable_id += 1;
-        let initial_k_arg = DefinitionInfo {
-            definition: None,
-            definition_id: DefinitionId(self.next_variable_id),
-            name: None,
-        };
+        let initial_k_arg =
+            DefinitionInfo { definition: None, definition_id: DefinitionId(self.next_variable_id), name: None };
         Ast::Lambda(Lambda {
             args: vec![initial_k_arg.clone()],
             body: Box::new(Ast::Variable(initial_k_arg)),
@@ -88,11 +79,28 @@ impl Context {
 
     fn fresh_var(&mut self) -> Variable {
         self.next_variable_id += 1;
-        DefinitionInfo {
-            definition: None,
-            definition_id: DefinitionId(self.next_variable_id),
-            name: None,
+        DefinitionInfo { definition: None, definition_id: DefinitionId(self.next_variable_id), name: None }
+    }
+
+    fn cps_variable(&mut self, variable: &hir::Variable, k: Ast) -> Ast {
+        let v = variable.clone();
+
+        if !self.finished_definitions.contains(&variable.definition_id) {
+            self.finished_definitions.insert(variable.definition_id);
+
+            if let Some(definition) = &variable.definition {
+                self.cps(&definition, k)
+            } else {
+                make_call(k, vec![v.into()])
+            }
+        } else {
+            make_call(k, vec![v.into()])
         }
+    }
+
+    fn cps_definition(&mut self, definition: &hir::Definition, k: Ast) -> Ast {
+        self.finished_definitions.insert(definition.variable);
+        self.cps(&definition.expr, k)
     }
 
     fn cps_lambda(&mut self, lambda: &Lambda) -> Ast {
@@ -152,7 +160,7 @@ impl Context {
         self.cps(&access.lhs, k)
     }
 
-    fn cps_tuple(&mut self, tuple: &crate::hir::Tuple, mut k: Ast) -> Ast {
+    fn cps_tuple(&mut self, tuple: &hir::Tuple, mut k: Ast) -> Ast {
         let fresh_arg_names = fmap(&tuple.fields, |_| self.fresh_var());
         let fresh_args = fmap(&fresh_arg_names, |arg| Ast::Variable(arg.clone()));
 
@@ -164,5 +172,28 @@ impl Context {
         }
 
         k
+    }
+
+    fn cps_sequence(&mut self, sequence: &hir::Sequence, mut k: Ast) -> Ast {
+        let fresh_arg_names = fmap(&sequence.statements, |_| self.fresh_var());
+
+        for (name, statement) in fresh_arg_names.into_iter().rev().zip(sequence.statements.iter().rev()) {
+            k = make_lambda(name, k);
+            k = self.cps(statement, k)
+        }
+
+        k
+    }
+
+    fn cps_if_expr(&mut self, if_expr: &hir::If, mut k: Ast) -> Ast {
+        let then = Box::new(self.cps(&if_expr.then, k.clone()));
+        let otherwise = Box::new(self.cps(&if_expr.otherwise, k));
+
+        let cond_var = self.fresh_var();
+        let condition = Box::new(Ast::Variable(cond_var.clone()));
+        let result_type = if_expr.result_type.clone();
+        k = make_lambda(cond_var, Ast::If(hir::If { condition, then, otherwise, result_type }));
+
+        self.cps(&if_expr.condition, k)
     }
 }
--- src/effects/redex.rs
+++ src/effects/redex.rs
@@ -1,21 +1,17 @@
+use crate::hir::Ast;
+use crate::hir::DefinitionId;
 use crate::hir::FunctionCall;
 use crate::hir::Lambda;
-use crate::util::fmap;
 use crate::hir::MemberAccess;
-use crate::hir::DefinitionId;
-use crate::hir::Ast;
 use crate::hir::Tuple;
+use crate::util::fmap;
 
 pub fn simplify(ast: Ast) -> Ast {
     match ast {
         Ast::Literal(_) => ast,
         Ast::Variable(_) => ast,
         Ast::Lambda(lambda) => {
-            Ast::Lambda(Lambda {
-                args: lambda.args,
-                body: Box::new(simplify(*lambda.body)),
-                typ: lambda.typ,
-            })
+            Ast::Lambda(Lambda { args: lambda.args, body: Box::new(simplify(*lambda.body)), typ: lambda.typ })
         },
         Ast::FunctionCall(call) => match *call.function {
             Ast::Lambda(lambda) => {
@@ -25,11 +21,7 @@ pub fn simplify(ast: Ast) -> Ast {
             function => {
                 let function = Box::new(simplify(function));
                 let args = fmap(call.args, simplify);
-                Ast::FunctionCall(FunctionCall {
-                    function,
-                    args,
-                    function_type: call.function_type,
-                })
+                Ast::FunctionCall(FunctionCall { function, args, function_type: call.function_type })
             },
         },
         Ast::Definition(_) => todo!(),
@@ -40,40 +32,32 @@ pub fn simplify(ast: Ast) -> Ast {
         Ast::Extern(_) => ast,
         Ast::Assignment(_) => todo!(),
         Ast::MemberAccess(access) => {
-            Ast::MemberAccess(MemberAccess {
-                lhs: Box::new(simplify(*access.lhs)),
-                member_index: access.member_index,
-            })
+            Ast::MemberAccess(MemberAccess { lhs: Box::new(simplify(*access.lhs)), member_index: access.member_index })
         },
         Ast::Tuple(tuple) => Ast::Tuple(Tuple { fields: fmap(tuple.fields, simplify) }),
         Ast::ReinterpretCast(_) => todo!(),
-        Ast::Builtin(_) => todo!(),
+        Ast::Builtin(builtin) => Ast::Builtin(builtin),
     }
 }
 
 fn substitute(ast: Ast, substitutions: &[(DefinitionId, &Ast)]) -> Ast {
     match ast {
         ast @ Ast::Literal(_) => ast,
-        Ast::Variable(var) => {
-            substitutions.iter()
-                .find(|(key, _)| *key == var.definition_id)
-                .map(|(_, new_value)| (*new_value).clone())
-                .unwrap_or(Ast::Variable(var))
-        }
-        Ast::Lambda(lambda) => {
-            Ast::Lambda(Lambda {
-                args: lambda.args,
-                body: Box::new(substitute(*lambda.body, substitutions)),
-                typ: lambda.typ,
-            })
-        },
-        Ast::FunctionCall(call) => {
-            Ast::FunctionCall(FunctionCall {
-                function: Box::new(substitute(*call.function, substitutions)),
-                args: fmap(call.args, |arg| substitute(arg, substitutions)),
-                function_type: call.function_type,
-            })
-        },
+        Ast::Variable(var) => substitutions
+            .iter()
+            .find(|(key, _)| *key == var.definition_id)
+            .map(|(_, new_value)| (*new_value).clone())
+            .unwrap_or(Ast::Variable(var)),
+        Ast::Lambda(lambda) => Ast::Lambda(Lambda {
+            args: lambda.args,
+            body: Box::new(substitute(*lambda.body, substitutions)),
+            typ: lambda.typ,
+        }),
+        Ast::FunctionCall(call) => Ast::FunctionCall(FunctionCall {
+            function: Box::new(substitute(*call.function, substitutions)),
+            args: fmap(call.args, |arg| substitute(arg, substitutions)),
+            function_type: call.function_type,
+        }),
         Ast::Definition(_) => todo!(),
         Ast::If(_) => todo!(),
         Ast::Match(_) => todo!(),
@@ -81,14 +65,12 @@ fn substitute(ast: Ast, substitutions: &[(DefinitionId, &Ast)]) -> Ast {
         Ast::Sequence(_) => todo!(),
         Ast::Extern(_) => todo!(),
         Ast::Assignment(_) => todo!(),
-        Ast::MemberAccess(access) => {
-            Ast::MemberAccess(MemberAccess {
-                lhs: Box::new(substitute(*access.lhs, substitutions)),
-                member_index: access.member_index,
-            })
-        },
+        Ast::MemberAccess(access) => Ast::MemberAccess(MemberAccess {
+            lhs: Box::new(substitute(*access.lhs, substitutions)),
+            member_index: access.member_index,
+        }),
         Ast::Tuple(tuple) => Ast::Tuple(Tuple { fields: fmap(tuple.fields, |field| substitute(field, substitutions)) }),
         Ast::ReinterpretCast(_) => todo!(),
-        Ast::Builtin(_) => todo!(),
+        Ast::Builtin(builtin) => Ast::Builtin(builtin),
     }
 }
